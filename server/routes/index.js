5a13ad061d5a4a771655ac86cd5543c5ed49d9e7
daehoon go <rhgo1749@naver.com>
2019-09-23 16:29:57 +0900

    html에서 태그 제거

--- server/routes/index.js
+++ server/routes/index.js
@@ -16,11 +16,14 @@ const getDOMFromURI = uri => {
    });
 }
 
+//https://webisfree.com/2015-12-22/[%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8]-%EC%A0%95%EA%B7%9C%ED%91%9C%ED%98%84%EC%8B%9D%EC%9D%84-%EC%82%AC%EC%9A%A9%ED%95%98%EC%97%AC-%ED%83%9C%EA%B7%B8%EB%A7%8C-%EC%A0%9C%EA%B1%B0%ED%95%98%EA%B8%B0
 router.post('/getbodytext', function(req, res, next) {
   const url = req.body.url;
   request(url, function (error, response, body) {
     console.log('body:', body); // Print the HTML for the Google homepage.
-    res.send(body);
+    // 정규표현식으로 태그 제거
+    var re = body.replace(/(<([^>]+)>)/ig, "");
+    res.send(re);
   });
 });
 
