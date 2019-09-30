3354b66ea290c9400a0f40967956eb56e706832d
Sain-Tech <chyh1900@gmail.com>
2019-09-30 22:34:27 +0900

    Revert "Revert "Revert "파이썬 결과 통신 시험중"""
    This reverts commit ff310bf4349dd1f4eecc9f5784bd969bb3faa657.

--- server/pyscripts/test.py
+++ server/pyscripts/test.py
@@ -1,9 +1,5 @@
 import sys
 
-# print('sys.argv 길이 : ', len(sys.argv))
-# for arg in sys.argv: 
-#     print('arg value = ', arg)
-
-while True:
-    url = input()
-    print(url)
+print('sys.argv 길이 : ', len(sys.argv))
+for arg in sys.argv: 
+    print('arg value = ', arg) 
